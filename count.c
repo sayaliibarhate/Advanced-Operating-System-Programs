007d27cd8d2a48801b303032f957681167c24f14
Sayali Barhate <161739375+sayalii27@users.noreply.github.com>
2024-03-21 20:07:09 +0530

    Update count.c

--- count.c
+++ count.c
@@ -1,3 +1,10 @@
+/*Write a C program that behaves like a shell (command interpreter). It has its own prompt 
+say “NewShell$”. Any normal shell command is executed from your shell by starting a 
+child process to execute the system program corresponding to the command. It should 
+additionally interpret the following command. 
+i) count c <filename> - print number of characters in file 
+ii) count w <filename> - print number of words in file 
+iii) count l <filename> - print number of lines in file */
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
