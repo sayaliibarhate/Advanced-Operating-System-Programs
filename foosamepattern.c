2c2e813b5e469dc2ecae2f16c2f2f8d9b7617f1d
Sayali Barhate <161739375+sayalii27@users.noreply.github.com>
2024-03-21 20:14:02 +0530

    Update foosamepattern.c

--- foosamepattern.c
+++ foosamepattern.c
@@ -1,3 +1,6 @@
+/*Write a C program that a string as an argument and return all the files that begins with 
+that name in the current directory. For example > ./a.out foo will return all file names that 
+begins with foo.*/
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
