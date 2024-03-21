692b41be85a9d083fe1e5aa9c30a220c37eb38fa
Sayali Barhate <161739375+sayalii27@users.noreply.github.com>
2024-03-21 20:13:02 +0530

    Update mergealltxt.c

--- mergealltxt.c
+++ mergealltxt.c
@@ -1,3 +1,5 @@
+/*Write a C program to read all txt files (that is files that ends with .txt) in the current 
+directory and merge them all to one txt file and returns a file descriptor for the new file*/
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
